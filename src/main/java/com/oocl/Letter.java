e978eda95e7ed62bca9c955e47272fd863221582
BaleLin <bale.lin@oocl.com>
2018-08-23 14:26:32 +0800

    [bale,quinn]refactor version2

--- src/main/java/com/oocl/Letter.java
+++ src/main/java/com/oocl/Letter.java
@@ -4,22 +4,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class Letter {
     public static List<String> permute(String input) {
 
-        if (input.length() >1) {
-            List<String> permutation = new ArrayList<String>();
-
-
-            for (int index = 0; index < input.length(); index++) {
-                List<String> temp = permute(removeCharByIndex(input, index));
-                for (int j = 0; j < temp.size(); j++) {
-                    permutation.add(input.substring(index, index + 1) + temp.get(j));
-                }
-            }
-            return permutation;
+        if (input.length() == 1) {
+            return Arrays.asList(input);
         }
-        return Arrays.asList(input);
+        List<String> permutation = new ArrayList<String>();
+        for (int index = 0; index < input.length(); index++) {
+            List<String> temp = permute(removeCharByIndex(input, index));
+            for (int j = 0; j < temp.size(); j++) {
+                permutation.add(input.substring(index, index + 1) + temp.get(j));
+            }
+        }
+        return permutation;
+
+
     }
 
     private static String removeCharByIndex(String input, int index) {
